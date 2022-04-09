91714386a4afacbe9fd9c3bde5e9c4b2dea1402f
rasuantaputra <rasuantaportofolio@gmail.com>
2022-04-09 17:09:51 +0700

    Instlasi library yang tidak terpakai memasukan gambar tanki

--- virtual_plant.py
+++ virtual_plant.py
@@ -1,12 +1,16 @@
+from email.mime import image
 import tkinter
 import matplotlib.pyplot as plt
-from pip import main
 import numpy as np
 
 main_window = tkinter.Tk()
 
 # Virtual plant frame size
 frame = tkinter.Canvas(main_window, bg="white", height=600, width=1000)
+
+img = tkinter.PhotoImage(file="img\Tank.png")
+frame.create_image(300,300, image=img)
+
 
 
 
