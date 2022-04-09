29eaaf45ba1784ebd95e65f5bd14ec133c48544b
rasuantaputra <rasuantaportofolio@gmail.com>
2022-04-09 16:15:38 +0700

    membuat judul

--- virtual_plant.py
+++ virtual_plant.py
@@ -5,11 +5,11 @@ import numpy as np
 
 main_window = tkinter.Tk()
 
-# Besar frame untuk virtual plant
+# Virtual plant frame size
 frame = tkinter.Canvas(main_window, bg="white", height=600, width=1000)
 
-# Membuat air dalam tank
-frame.create_water_tank(50, 50, 20, 100, fill="blue", outline = 'blue')
+
 
 frame.pack()
+main_window.title('Virtual Plant DTS Kominfo 2022')
 main_window.mainloop()
