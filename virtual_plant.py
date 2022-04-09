ab35c706bf410b6d626a84a34870cfc1cb5fe1ea
rasuantaputra <rasuantaportofolio@gmail.com>
2022-04-09 22:12:21 +0700

    menambahkan fungsi mouse_motion_coordinate untuk mengetahui koordinat melalui pergerakan mouse

--- virtual_plant.py
+++ virtual_plant.py
@@ -1,26 +1,38 @@
 import tkinter
 from venv import create
-import matplotlib.pyplot as plt
-import numpy as np
 
 main_window = tkinter.Tk()
 
 # Virtual plant frame size
 frame = tkinter.Canvas(main_window, bg="white", height=600, width=1000)
-
-# Insert tank image
+# take Tank.png image
 img = tkinter.PhotoImage(file="img\Tank.png")
-frame.create_image(320,360, image=img)
 
-# Make linquid inside tank
+# mose motion function for definding coordinate
+def mouse_motion_coordinate(event):
+    x, y = event.x, event.y
+    print('{}, {}'.format(x, y))
+
+def tank_image():
+    # Insert tank image
+    frame.create_image(320,360, image=img)
+    # valve 1 (x1, y1, x2, y2, x3, y3, x4, y4, x5, y5)
+    # frame.create_polygon(x1, y1, x2, y2, x3, y3, x4, y4, x5, y5)
+    # valve 2 (x1, y1, x2, y2, x3, y3, x4, y4, x5, y5)
+    # frame.create_polygon()
+    
 # liquid = (level - x)/level
 def liquid(level=310):
-    # frame.create_rectangle(411, 600, 203, 300, fill="blue", outline = 'blue')
-    # (x1, y1, x2, y2)
+    # Make linquid inside tank
     frame.create_rectangle(174, level, 383, 518, fill="blue", outline = 'blue')
 
+# =========try block========
+tank_image()
 liquid()
-
+# ==========================
 frame.pack()
+
+# Activate mouse corrdinate
+main_window.bind('<Motion>', mouse_motion_coordinate)
 main_window.title('Tugas DTS Kominfo 2022')
 main_window.mainloop()
