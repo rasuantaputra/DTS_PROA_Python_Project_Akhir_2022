2da82d1d53d20fd92b4fe669e0f64b792de193b5
rasuantaputra <rasuantaportofolio@gmail.com>
2022-05-01 18:17:05 +0700

    modifikasi virtual environtment

--- Scripts/activate.bat
+++ Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Belajar\Digital Talent Schoolarship\PROA Python 2022\Tugas\Proyek Akhir\DTS_PROA_Python_Project_Akhir
+set VIRTUAL_ENV=D:\Belajar\Digital Talent Schoolarship\PROA Python 2022\Tugas\Proyek Akhir\DTS_PROA_Python_Project_Akhir_2022
 
 if not defined PROMPT set PROMPT=$P$G
 
